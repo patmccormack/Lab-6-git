5e8b7e24e8d580a270e26c3df477ed3e1d8a79e1
singsian <paddymcco@gmail.com>
2023-07-10 18:22:13 -0400

    add encoder.py

---
+++ encoder.py
@@ -0,0 +1,30 @@
+passencode = {'0': '3', '1': '4', '2': '5', '3': '6',
+              '4': '7', '5': '8', '6': '9', '7': '0',
+              '8': '1', '9': '2'}
+
+passdecode = {'0': '7', '1': '8', '2': '9', '3': '0',
+              '4': '1', '5': '2', '6': '3', '7': '4',
+              '8': '5', '9': '6'}
+
+
+def encode(code):
+    y = list(code)
+    x = ''
+    for digit in y:
+        x += passencode[digit]
+    return x
+
+
+if __name__ == "__main__":
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        user_input = int(input("Please enter an option: "))
+
+        if user_input == 1:
+            encode_input = input("Please enter your password to encode: ")
+            print(f"Your password has been encoded and stored! {encode(encode_input)} \n")
